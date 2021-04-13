b5ba12e639a2cb190a103afae54435fcf3dfbd25
Cameron Smith <theauspiciouscammy@gmail.com>
2021-04-13 07:42:39 -0700

    4/13 commit

--- src/com/cameron/algos/Algo.java
+++ src/com/cameron/algos/Algo.java
@@ -2,6 +2,7 @@ package com.cameron.algos;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Algo {
 	//Print 1 - 255
@@ -25,42 +26,42 @@ public class Algo {
 		return sum;
 	}
 	//Iterate through array
-	public void printValues( int[] myList) {
-		for(int i = 0; i < myList.length; i++) {
-			System.out.println(myList[i]);
+	public void printValues( int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
 	} 
 	//Find Max
-	public int findMax(int[] myList) {
-		int max = myList[0];
-		for(int i = 0; i < myList.length; i++) {
-			if(myList[i] > max) {
-				max = myList[i];
+	public int findMax(int[] arr) {
+		int max = arr[0];
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
 			}
 		}
 		return max;
 	}
 	//Find Avgerage
-	public double findAvg(double[] myList) {
+	public double findAvg(double[] arr) {
 		double sum = 0;
-		for(int i = 0; i < myList.length; i++) {
-			sum += myList[i];
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
 		}
-		double avg = sum / myList.length;
+		double avg = sum / arr.length;
 		return avg;
 	}
 	//List With Odd Numbers
 	public ArrayList<Integer> allOdds(){
-		ArrayList<Integer> newList = new ArrayList<Integer>();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
 		for(int i = 1; i <= 255; i+=2) {
-			newList.add(i);
+			arr.add(i);
 		}
-		return newList;
+		return arr;
 	}
 	//Greater Than Y
-	public int greaterThan(int[] myList, int y) {
+	public int greaterThan(int[] arr, int y) {
 		int sum = 0;
-		for(int i : myList) {
+		for(int i : arr) {
 			if(i > y) {
 				sum += 1;
 			}
@@ -68,20 +69,72 @@ public class Algo {
 		return sum;
 	}
 	//Square the Values
-	public String squareValues(int[] myList) {
-		for( int i = 0; i < myList.length; i++) {
-			myList[i] *= myList[i];
+	public String squareValues(int[] arr) {
+		for( int i = 0; i < arr.length; i++) {
+			arr[i] *= arr[i];
 		}
-		return Arrays.toString(myList);
+		return Arrays.toString(arr);
 	}
 	//Eliminate Negative Numbers
-	public String elimNegative(int[] myList) {
-		for( int i = 0; i < myList.length; i++) {
-			if( myList[i] < 0) {
-				myList[i] = 0;
+	public String elimNegative(int[] arr) {
+		for( int i = 0; i < arr.length; i++) {
+			if( arr[i] < 0) {
+				arr[i] = 0;
 			}
 		}
-		return Arrays.toString(myList);
+		return Arrays.toString(arr);
+	}
+	//Max, Min, and Average
+	public void maxMinAvg(double[] arr) {
+		double max = arr[0];
+		double min = arr[0];
+		double sum = 0;
+		for( double i : arr) {
+			if(i > max) {
+				max = i;
+			}
+			if(i < min) {
+				min = i;
+			}
+			sum += i;
+		}
+		double avg = sum / arr.length;
+		System.out.println("Max: "+max+" Min: "+min+" Avg: "+avg);
+	}
+	//Shifting the values in the array
+	public String getShwifty(int[] arr) {
+		for(int i = 0; i < arr.length -1; i++) {
+			int temp = arr[i];
+			@SuppressWarnings("unused")
+			int next = arr[i+1];
+			arr[i] = arr[i+1];
+			next = temp;
+		}
+		arr[arr.length-1] = 0;
+		return Arrays.toString(arr);
+	}
+	//Random Array
+	public ArrayList<Integer> randomArray(){
+		ArrayList<Integer> bounds = new ArrayList<Integer>();
+		for(int i = 1; i <= 100; i++){
+			bounds.add(i);
+        }
+        Random random = new Random();
+        ArrayList<Integer> randomTen = new ArrayList<Integer>();
+        for( int i = 0; i < 10; i++){
+            int randomNum = random.nextInt(90);
+            randomTen.add(bounds.get(randomNum));
+        }
+        return randomTen;
+    } 
+	//Swapping two values
+	public String swapperoo(int[] input) {
+		for(int i = 0; i < input.length; i++) {
+			int temp = input[0];
+			input[0] = input[input.length-1];
+			input[input.length-1] = temp;
+		}
+		return Arrays.toString(input);
 	}
 }
 
--- src/com/cameron/algos/AlgosController.java
+++ src/com/cameron/algos/AlgosController.java
@@ -3,18 +3,22 @@ package com.cameron.algos;
 public class AlgosController {
 	public static void main(String[] args) {
 		Algo algorithm = new Algo();
+		int[] myArr = {2,-3,5,7,9,-4};
+		//double[] myArr1 = {1,3,5,7,20};
 		//algorithm.printNums();
 		//algorithm.printOdds();
 		//System.out.println(algorithm.sumOdds());
-		int[] myList = {2,-3,5,7,9,-4};
-		//algorithm.printValues(myList);
-		//System.out.println(algorithm.findMax(myList));
-		//double[] myList1 = {1,3,5,7,20};
-		//System.out.println(algorithm.findAvg(myList1));
+		//algorithm.printValues(myArr);
+		//System.out.println(algorithm.findMax(myArr));
+		//System.out.println(algorithm.findAvg(myArr1));
 		//System.out.println(algorithm.allOdds());
 		//System.out.println(algorithm.greaterThan(myList, 10));
-		System.out.println(algorithm.squareValues(myList));
-		//System.out.println(algorithm.elimNegative(myList));
+		//System.out.println(algorithm.squareValues(myArr));
+		//System.out.println(algorithm.elimNegative(myArr));
+		//algorithm.maxMinAvg(myArr1);
+		//System.out.println(algorithm.getShwifty(myArr));
+		//System.out.println(algorithm.randomArray());
+		System.out.println(algorithm.swapperoo(myArr));
 	}
 
 }
